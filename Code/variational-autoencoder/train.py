dee69c64f2293e0f422513b133499984b9da07e1
serendipityerr <panmk@shanghaitech.edu.cn>
2024-12-30 00:42:25 +0800

    code

--- Code/variational-autoencoder/train.py
+++ Code/variational-autoencoder/train.py
@@ -4,13 +4,13 @@ import seaborn as sns
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
+from torchvision.utils import make_grid
 import torchvision
 import os
 
 from torch.autograd import Variable
 from torch.utils.data.dataloader import DataLoader
 from torchvision.datasets import MNIST
-from torchvision.utils import make_grid as make_image_grid
 from tqdm import *
 
 from vae_model import VAE
@@ -19,23 +19,28 @@ torch.manual_seed(2017)
 sns.set_style('dark')
 
 def criterion(x_out, x_in, z_mu, z_logvar):
-    bce_loss = F.binary_cross_entropy(x_out,x_in,size_average=False)
+    bce_loss = F.binary_cross_entropy(x_out, x_in, size_average=False)
     kld_loss = -0.5 * torch.sum(1 + z_logvar - (z_mu ** 2) - torch.exp(z_logvar))
     loss = (bce_loss + kld_loss) / x_out.size(0) # normalize by batch size
     return loss
 
 # Train the model
-def train(model, optimizer, dataloader, epochs=15):
+def train(model, optimizer, dataloader, epochs=100):
     losses = []
     for _ in tqdm(range(epochs), desc='Epochs'):
-        for images,_ in dataloader:
+        l = 0
+        cnt = 0
+        for images, _ in dataloader:
             x_in = Variable(images)
             optimizer.zero_grad()
             x_out, z_mu, z_logvar = model(x_in)
             loss = criterion(x_out, x_in, z_mu, z_logvar)
             loss.backward()
             optimizer.step()
-            losses.append(loss.data)
+            l += loss.data
+            # losses.append(loss.data)
+            cnt += 1
+        losses.append(l / cnt)
     return losses
 
 # Test the model
@@ -48,30 +53,89 @@ def test(model, dataloader):
         running_loss = running_loss + (loss.data * x_in.size(0))
     return running_loss / len(dataloader.dataset)
 
-def visualize_loss(loss):
+def visualize_loss(loss, epoch):
     plt.figure(figsize=(10, 5))
     plt.plot(loss)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.savefig(f'sample/{epoch}_epoch_loss.png', dpi = 300)
     plt.show()
 
 # Save the results
-def visualize_mnist_vae(model, dataloader, num=16, sample_dir="./sample"):
+def visualize_mnist_vae_generation(model, dataloader, index, num=16, sample_dir="./sample"):
     def imshow(img):
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.axis('off')
         plt.show()
     
+    LQ_path = os.path.join(sample_dir, "LQ_generation")
+    HQ_path = os.path.join(sample_dir, "HQ_generation")
     if not os.path.exists(sample_dir):
         os.mkdir(sample_dir)
+    if not os.path.exists(LQ_path):
+        os.mkdir(LQ_path)
+    if not os.path.exists(HQ_path):
+        os.mkdir(HQ_path)
+
     images, _ = next(iter(dataloader))
     images = images[0:num,:,:]
     x_in = Variable(images)
-    x_out, _, _ = model(x_in)
+    noise = torch.randn_like(x_in)
+    # x_out, _, _ = model(x_in)
+    x_out, _, _ = model(noise)
     x_out = x_out.data
-    torchvision.utils.save_image(images, os.path.join(sample_dir, "input.png"))
-    torchvision.utils.save_image(x_out, os.path.join(sample_dir, "output.png"))
-    imshow(make_image_grid(images))
-    imshow(make_image_grid(x_out))
+
+
+    grid_LQ = make_grid(x_out, nrow=4)
+    img_LQ = torchvision.transforms.ToPILImage()(grid_LQ)
+    img_LQ.save(os.path.join(LQ_path, f"{index}.png"))
+    img_LQ.close()
+
+    grid_HQ = make_grid(images, nrow=4)
+    img_HQ = torchvision.transforms.ToPILImage()(grid_HQ)
+    img_HQ.save(os.path.join(HQ_path, f"{index}.png"))
+    img_HQ.close()
+    # torchvision.utils.save_image(images, os.path.join(HQ_path, f"{index}.png"))
+    # torchvision.utils.save_image(x_out, os.path.join(LQ_path, f"{index}.png"))
+    # imshow(make_image_grid(images))
+    # imshow(make_image_grid(x_out))
+
+def visualize_mnist_vae_reconstruction(model, dataloader, index, num=16, sample_dir="./sample"):
+    def imshow(img):
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.axis('off')
+        plt.show()
+    
+    LQ_path = os.path.join(sample_dir, "LQ_reconstruction")
+    HQ_path = os.path.join(sample_dir, "HQ_reconstruction")
+    if not os.path.exists(sample_dir):
+        os.mkdir(sample_dir)
+    if not os.path.exists(LQ_path):
+        os.mkdir(LQ_path)
+    if not os.path.exists(HQ_path):
+        os.mkdir(HQ_path)
+
+    images, _ = next(iter(dataloader))
+    images = images[0:num,:,:]
+    x_in = Variable(images)
+    # noise = torch.randn_like(x_in)
+    x_out, _, _ = model(x_in)
+    # x_out, _, _ = model(noise)
+    x_out = x_out.data
+
+    grid_LQ = make_grid(x_out, nrow=4)
+    img_LQ = torchvision.transforms.ToPILImage()(grid_LQ)
+    img_LQ.save(os.path.join(LQ_path, f"{index}.png"))
+    img_LQ.close()
+
+    grid_HQ = make_grid(images, nrow=4)
+    img_HQ = torchvision.transforms.ToPILImage()(grid_HQ)
+    img_HQ.save(os.path.join(HQ_path, f"{index}.png"))
+    img_HQ.close()
+    # imshow(make_image_grid(images))
+    # imshow(make_image_grid(x_out))
 
 
 def main():
@@ -80,19 +144,20 @@ def main():
     # Data loaders
     trainloader = DataLoader(
         MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor()),
-        batch_size=128, shuffle=True)
+        batch_size=64, shuffle=True)
     testloader = DataLoader(
         MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor()),
-        batch_size=128, shuffle=True)
+        batch_size=64, shuffle=True)
     
-    train_losses = train(model, optimizer, trainloader)
-    visualize_loss(train_losses)
+    epoch = 200
+    train_losses = train(model, optimizer, trainloader, epoch)
+    visualize_loss(train_losses, epoch)
 
     # Testing
     # test_loss = test(model,testloader)
     # print(test_loss)
-
-    visualize_mnist_vae(model,testloader)
+    visualize_mnist_vae_generation(model, testloader, epoch)
+    visualize_mnist_vae_reconstruction(model, testloader, epoch)
 
 if __name__ == '__main__':
     main()
--- Code/variational-autoencoder/vae_model.py
+++ Code/variational-autoencoder/vae_model.py
@@ -1,45 +1,39 @@
-import seaborn as sns
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.utils import make_grid as make_image_grid
 
-torch.manual_seed(2017) # reproducability
-sns.set_style('dark')
+torch.manual_seed(2017)
 
 class VAE(nn.Module):
-    def __init__(self,latent_dim=20,hidden_dim=500):
-        super(VAE,self).__init__()
-        self.fc_e = nn.Linear(784,hidden_dim)
-        self.fc_mean = nn.Linear(hidden_dim,latent_dim)
-        self.fc_logvar = nn.Linear(hidden_dim,latent_dim)
-        self.fc_d1 = nn.Linear(latent_dim,hidden_dim)
-        self.fc_d2 = nn.Linear(hidden_dim,784)
+    def __init__(self, latent_dim = 20, hidden_dim = 500):
+        super(VAE, self).__init__()
+        self.fc_e = nn.Linear(784, hidden_dim)
+        self.fc_mean = nn.Linear(hidden_dim, latent_dim)
+        self.fc_logvar = nn.Linear(hidden_dim, latent_dim)
+        self.fc_d1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc_d2 = nn.Linear(hidden_dim, 784)
             
-    def encoder(self,x_in):
-        x = F.relu(self.fc_e(x_in.view(-1,784)))
+    def encoder(self, x_in):
+        x = F.relu(self.fc_e(x_in.view(-1, 784)))
         mean = self.fc_mean(x)
         logvar = self.fc_logvar(x)
         return mean, logvar
     
-    def decoder(self,z):
+    def decoder(self, z):
         z = F.relu(self.fc_d1(z))
         x_out = F.sigmoid(self.fc_d2(z))
-        return x_out.view(-1,1,28,28)
+        return x_out.view(-1, 1, 28, 28)
     
     def sample_normal(self,mean,logvar):
-        # Using torch.normal(means,sds) returns a stochastic tensor which we cannot backpropogate through.
-        # Instead we utilize the 'reparameterization trick'.
-        # http://stats.stackexchange.com/a/205336
-        # http://dpkingma.com/wordpress/wp-content/uploads/2015/12/talk_nips_workshop_2015.pdf
-        sd = torch.exp(logvar*0.5)
+        sd = torch.exp(logvar * 0.5)
         e = Variable(torch.randn(sd.size())) # Sample from standard normal
         z = e.mul(sd).add_(mean)
         return z
     
     def forward(self,x_in):
         z_mean, z_logvar = self.encoder(x_in)
-        z = self.sample_normal(z_mean,z_logvar)
+        z = self.sample_normal(z_mean, z_logvar)
         x_out = self.decoder(z)
         return x_out, z_mean, z_logvar
